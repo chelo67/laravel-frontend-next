29d05067f3d8042f41012b8c11dd8994fdc0f17c
chelo67 <marceloacampora@gmail.com>
2025-03-17 20:56:35 -0300

    1:40:02

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,34 +1,98 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
+
+interface formData{
+    name?: string;
+    email: string;
+    password: string;
+    password_confirmation?: string;
+}
 
 const Auth: React.FC = () => {
-    return <>
-         <div className="container d-flex justify-content-center align-items-center vh-100">
-        <div className="card p-4" style={{width: "400px"}}>
-            {/* <!-- Login Form --> */}
-            <h3 className="text-center">Login</h3>
-            <form>
-                <input className="form-control mb-2" name="email" type="email" placeholder="Email" required />
-                <input className="form-control mb-2" name="password" type="password" placeholder="Password" required />
-                <button className="btn btn-primary w-100" type="submit">Login</button>
-            </form>
+    const [isLogin, setIslogin] = useState<boolean>(true);
+    const [formData, setFormData] = useState<formData>({
+        name: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+    });
 
-            <p className="mt-3 text-center">Don't have an account? Register</p>
-            
-            <hr />
+    const handleOnChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        });
+    };
 
-            {/* <!-- Register Form --> */}
-            <h3 className="text-center">Register</h3>
-            <form>
-                <input className="form-control mb-2" name="name" type="text" placeholder="Name" required />
-                <input className="form-control mb-2" name="email" type="email" placeholder="Email" required />
-                <input className="form-control mb-2" name="password" type="password" placeholder="Password" required />
-                <input className="form-control mb-2" name="password_confirmation" type="password" placeholder="Confirm Password" required />
-                <button className="btn btn-primary w-100" type="submit">Register</button>
-            </form>
+    const handleFormSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        if(isLogin){
+            alert("Login submitted");
+        } else {
+            alert("Register submitted");
+        }
+    };
+
+    return (
+        <div className="container d-flex justify-content-center align-items-center vh-100">
+            <div className="card p-4" style={{width: "400px"}}>
+                <h3 className="text-center">{ isLogin ? "Login" : "Register" }</h3>
+                <form onSubmit={handleFormSubmit}>
+                    {!isLogin && (
+                        <input 
+                            className="form-control mb-2" 
+                            name="name" 
+                            type="text"
+                            value={formData.name}
+                            onChange={handleOnChangeInput}
+                            placeholder="Name" 
+                            required 
+                        />
+                    )}
+                    
+                    <input 
+                        className="form-control mb-2" 
+                        name="email" 
+                        type="email"
+                        value={formData.email}
+                        onChange={handleOnChangeInput}
+                        placeholder="Email" 
+                        required 
+                    />
+                    <input 
+                        className="form-control mb-2" 
+                        name="password" 
+                        type="password"
+                        value={formData.password}
+                        onChange={handleOnChangeInput}
+                        placeholder="Password" 
+                        required 
+                    />
+                    {!isLogin && (
+                        <input 
+                            className="form-control mb-2" 
+                            name="password_confirmation" 
+                            type="password"
+                            value={formData.password_confirmation}
+                            onChange={handleOnChangeInput}
+                            placeholder="Confirm Password" 
+                            required 
+                        />
+                    )}
+                    
+                    <button className="btn btn-primary w-100" type="submit">{ isLogin ? "Login" : "Register" }</button>
+                </form>
+
+                <p className="mt-3 text-center">
+                    {isLogin ? "Don't have an account? " : "Already have an account "}
+                    <span onClick={() => setIslogin(!isLogin)} style={{ cursor: "pointer"}}>
+                        {isLogin ? "Register" : "Login"}
+                    </span>
+                </p>
+            </div>
         </div>
-    </div>
-    </>
-   
+    );
 }
 
 export default Auth;
---
+++ capitulos.md
@@ -0,0 +1,4 @@
+# Build a Full-Stack CRUD App with Next.js & Laravel 12 | Full 3-Hour Course
+## Online Web Tutor
+
+1:17:26
